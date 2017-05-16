7dea41a6f695a5fa3c41e02d8d6f50bda6c91a32
idonlakyo <daniel.lisq12@gmail.com>
2017-05-15 20:34:51 -0400

    Update File Processing Assignment.py

--- File Processing Assignment.py
+++ File Processing Assignment.py
@@ -18,6 +18,16 @@ while x != len(textlist) - 1:
         else:
             textlist[x] = "z"
         textlist.remove(textlist[x+1])
+    if textlist[x].lower() == "a" and textlist[x+1] == " ":
+        if textlist[x] == "A"
+            textlist[x] = "E"
+        if textlist[x] == "a":
+            textlist[x] = "e"
+        temptext = textlist[x+1:]
+        textlist[x+1] = "i"
+        textlist[x+2] = "n"
+        for i in range(len(textlist[x+3:])):
+            textlist[i-2] = templist[i]
     x += 1
 text.seek(0)
 text.write("".join(textlist))
